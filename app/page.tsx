5f50abfdc78a26a73fec76c7275ff06817b77098
Jerry-BloodBerry <kcwikowski007@yahoo.com>
2025-01-22 17:28:05 +0100

    feat: finish work with us section

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,14 @@ import onSiteTranslationsImg from '@/public/images/on-site-translations.jpg'
 import qualityControlImg from '@/public/images/quality-control.png'
 import translatingPic from '@/public/images/translating.jpg'
 import voipPhoneTranslationsImg from '@/public/images/voip-phone-conference-translations.jpg'
-import { DocumentTextIcon } from '@heroicons/react/24/outline'
+import {
+  CalendarDaysIcon,
+  CheckIcon,
+  DocumentTextIcon,
+} from '@heroicons/react/24/outline'
 
 import Service from '@/components/service'
+import WorkWithUsStep from '@/components/work-with-us-step'
 
 export default function Home() {
   return (
@@ -152,56 +157,53 @@ export default function Home() {
       </div>
       <div
         id="working-with-us"
-        className="my-12 mx-6 lg:mx-auto h-full flex flex-col items-center justify-center"
+        className="my-12 mx-12 lg:mx-auto h-full flex flex-col items-center justify-center"
       >
         <div className="max-w-4xl w-full mx-6 xl:mx-0">
           <h2 className="text-3xl font-bold text-left">Working with us</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-            <WorkWithUsStep />
+          <p className="mt-6">
+            Working with us is easy! With all translations we follow a standard
+            process which is simple and easy to follow. It all comes down to
+            three steps:
+          </p>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-8 mt-6 justify-center">
+            <WorkWithUsStep>
+              <WorkWithUsStep.Icon icon={DocumentTextIcon} />
+              <WorkWithUsStep.Title>Send Your Documents</WorkWithUsStep.Title>
+              <WorkWithUsStep.Content>
+                Please send your documents to the following e-mail address:{' '}
+                <a
+                  href="mailto:translations.request@acme.com?subject=Translation request"
+                  className="text-[#150f47] underline"
+                >
+                  translations.request@acme.com
+                </a>
+                . After receiving the documents we will provide a quote within{' '}
+                <b>24 hours</b>.
+              </WorkWithUsStep.Content>
+            </WorkWithUsStep>
+            <WorkWithUsStep>
+              <WorkWithUsStep.Icon icon={CalendarDaysIcon} />
+              <WorkWithUsStep.Title>Set a deadline</WorkWithUsStep.Title>
+              <WorkWithUsStep.Content>
+                After receiving the quote and accepting the terms and price, we
+                will ask you to set a <b>deadline</b> for the translation.
+                Deadlines can be as short as a couple of hours for simple
+                documents!
+              </WorkWithUsStep.Content>
+            </WorkWithUsStep>
+            <WorkWithUsStep>
+              <WorkWithUsStep.Icon icon={CheckIcon} />
+              <WorkWithUsStep.Title>Receive Translation</WorkWithUsStep.Title>
+              <WorkWithUsStep.Content>
+                Your translation will be sent to you by means that you have
+                specified in the request. We are flexible and can have your
+                translation delivered to you by email, phone, post or carrier.
+              </WorkWithUsStep.Content>
+            </WorkWithUsStep>
           </div>
         </div>
       </div>
     </main>
   )
-}
-
-const WorkWithUsStep = () => {
-  return (
-    <div className="relative mt-10 max-w-[300px] px-4 pt-12 pb-8 mb-8 border-2 border-[#477050] rounded-xl">
-      <WorkWithUsIcon icon={DocumentTextIcon} />
-      <WorkWithUsTitle>Send Your Documents</WorkWithUsTitle>
-      <WorkWithUsContent>
-        Please send your documents to the following e-mail address:{' '}
-        <a
-          href="mailto:translations.request@acme.com?subject=Translation request"
-          className="text-[#150f47] underline"
-        >
-          translations.request@acme.com
-        </a>
-        . After receiving the translation we will provide a quote within{' '}
-        <b>24 hours</b>.
-      </WorkWithUsContent>
-    </div>
-  )
-}
-
-const WorkWithUsTitle = ({ children }: { children: React.ReactNode }) => {
-  return <div className="mt-4 text-lg font-bold text-center">{children}</div>
-}
-
-const WorkWithUsIcon = ({
-  icon,
-}: {
-  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>
-}) => {
-  const Icon = icon
-  return (
-    <div className="absolute -top-[40px] left-[50%] translate-x-[-50%] h-[90px] w-[90px] mx-auto bg-white border-[#477050] border-4 rounded-full text-black text-center">
-      <Icon className="size-12 text-black mx-auto mt-4" />
-    </div>
-  )
-}
-
-const WorkWithUsContent = ({ children }: { children: React.ReactNode }) => {
-  return <div className="mt-4 text-center">{children}</div>
 }
---
+++ components/work-with-us-step.tsx
@@ -0,0 +1,34 @@
+const WorkWithUsStep = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="relative mt-10 max-w-[300px] px-4 pt-12 pb-8 mb-8 mx-auto border-2 border-[#477050] bg-white rounded-xl">
+      {children}
+    </div>
+  )
+}
+
+const WorkWithUsTitle = ({ children }: { children: React.ReactNode }) => {
+  return <div className="mt-4 text-lg font-bold text-center">{children}</div>
+}
+
+const WorkWithUsIcon = ({
+  icon,
+}: {
+  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>
+}) => {
+  const Icon = icon
+  return (
+    <div className="absolute -top-[40px] left-[50%] translate-x-[-50%] h-[90px] w-[90px] mx-auto bg-white border-[#477050] border-4 rounded-full text-black text-center">
+      <Icon className="size-12 text-black mx-auto mt-4" />
+    </div>
+  )
+}
+
+const WorkWithUsContent = ({ children }: { children: React.ReactNode }) => {
+  return <div className="mt-4 text-center">{children}</div>
+}
+
+WorkWithUsStep.Title = WorkWithUsTitle
+WorkWithUsStep.Icon = WorkWithUsIcon
+WorkWithUsStep.Content = WorkWithUsContent
+
+export default WorkWithUsStep
