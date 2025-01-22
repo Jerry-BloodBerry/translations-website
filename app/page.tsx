868bb68f2abbc204ad15f85474a66f2c90721867
Jerry-BloodBerry <kcwikowski007@yahoo.com>
2025-01-22 16:47:39 +0100

    refactor: move service to its separate component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image, { StaticImageData } from 'next/image'
+import Image from 'next/image'
 import Link from 'next/link'
 import correctionImg from '@/public/images/correction-services.png'
 import onSiteTranslationsImg from '@/public/images/on-site-translations.jpg'
@@ -7,7 +7,7 @@ import translatingPic from '@/public/images/translating.jpg'
 import voipPhoneTranslationsImg from '@/public/images/voip-phone-conference-translations.jpg'
 import { DocumentTextIcon } from '@heroicons/react/24/outline'
 
-import { cn } from '@/lib/utils'
+import Service from '@/components/service'
 
 export default function Home() {
   return (
@@ -67,7 +67,7 @@ export default function Home() {
       <div id="services-intro" className="mt-10 py-10 bg-slate-50">
         <h2 className="text-3xl font-bold text-center mb-6">We Offer:</h2>
         <Service>
-          <ServiceImage
+          <Service.Image
             src={qualityControlImg}
             alt="We offer Correction and Quality Control services"
             width={680}
@@ -75,74 +75,79 @@ export default function Home() {
             variant="right"
             className="max-w-[200px] md:max-w-[200px]"
           />
-          <ServiceContent variant="left">
-            <ServiceTitle>High Quality Translations</ServiceTitle>
-            <ServiceDescription>
+          <Service.Content variant="left">
+            <Service.Title>High Quality Translations</Service.Title>
+            <Service.Description>
               Our translators are known for their attention to detail and always
               deliver timely results. We offer translations for several language
               pairs and always include proofing as part of our service.
-            </ServiceDescription>
-            <ServiceCTA url="/contact" urlText="Request translation" />
-          </ServiceContent>
+            </Service.Description>
+            <Service.CallToAction
+              url="/contact"
+              urlText="Request translation"
+            />
+          </Service.Content>
         </Service>
         <Service>
-          <ServiceImage
+          <Service.Image
             src={correctionImg}
             alt="We offer Correction and Quality Control services"
             width={680}
             height={850}
             variant="left"
           />
-          <ServiceContent variant="right">
-            <ServiceTitle>Correction Services</ServiceTitle>
-            <ServiceDescription>
+          <Service.Content variant="right">
+            <Service.Title>Correction Services</Service.Title>
+            <Service.Description>
               Already have a translation and need it to be checked by another
               translator? We have got you covered. We offer proofreading and
               correction services for all supported language pairs.
-            </ServiceDescription>
-            <ServiceCTA url="/language-pairs" urlText="Show pairs"></ServiceCTA>
-          </ServiceContent>
+            </Service.Description>
+            <Service.CallToAction url="/language-pairs" urlText="Show pairs" />
+          </Service.Content>
         </Service>
         <Service>
-          <ServiceImage
+          <Service.Image
             src={onSiteTranslationsImg}
             alt="We offer On-site and Cabin Translations"
             width={640}
             height={291}
             variant="right"
           />
-          <ServiceContent variant="left">
-            <ServiceTitle>
+          <Service.Content variant="left">
+            <Service.Title>
               On-site / Cabin
               <br />
               Translations
-            </ServiceTitle>
-            <ServiceDescription>
+            </Service.Title>
+            <Service.Description>
               Need an on-site translation? We can help you with that. We offer
               translations for hourly wages specified for each language pair.
               You can find the wages table by clicking the link below.
-            </ServiceDescription>
-            <ServiceCTA url="language-pairs" urlText="Show wages" />
-          </ServiceContent>
+            </Service.Description>
+            <Service.CallToAction url="language-pairs" urlText="Show wages" />
+          </Service.Content>
         </Service>
         <Service>
-          <ServiceImage
+          <Service.Image
             src={voipPhoneTranslationsImg}
             alt="We offer VoIP and Phone Conference translation services"
             width={640}
             height={291}
             variant="left"
           />
-          <ServiceContent variant="right">
-            <ServiceTitle>Phone/VoIP and Conference Translations</ServiceTitle>
-            <ServiceDescription>
+          <Service.Content variant="right">
+            <Service.Title>
+              Phone/VoIP and Conference Translations
+            </Service.Title>
+            <Service.Description>
               We offer translations for phone/video calls, on-line conferences
               and meetings with translation performed in real time. We also
               provide services related to adding subtitles to existing
               recordings.
-            </ServiceDescription>
-            <ServiceCTA url="contact" urlText="Contact Us" />
-          </ServiceContent>
+            </Service.Description>
+            <Service.CallToAction url="contact" urlText="Contact Us" />
+          </Service.Content>
         </Service>
       </div>
       <div
@@ -160,83 +165,6 @@ export default function Home() {
   )
 }
 
-const Service = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="max-w-full lg:max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-6 mt-16 mx-0 md:mx-6 lg:mx-auto">
-      {children}
-    </div>
-  )
-}
-
-interface ServiceImageProps {
-  src: StaticImageData
-  alt: string
-  width: number
-  height: number
-  variant: 'left' | 'right'
-  className?: string
-}
-
-const ServiceImage = (props: ServiceImageProps) => {
-  return (
-    <div
-      className={cn(
-        'order-last px-12 md:px-0',
-        props.variant == 'left' && 'md:order-first'
-      )}
-    >
-      <Image
-        src={props.src}
-        alt={props.alt}
-        width={props.width}
-        height={props.height}
-        className={cn(
-          'max-w-full md:max-w-[350px] mx-auto',
-          props.variant === 'left' ? 'lg:ml-0' : 'lg:mr-0',
-          props.className
-        )}
-      />
-    </div>
-  )
-}
-
-const ServiceTitle = ({ children }: { children: React.ReactNode }) => {
-  return <h3 className="text-2xl font-bold">{children}</h3>
-}
-
-const ServiceDescription = ({ children }: { children: React.ReactNode }) => {
-  return <p className="mt-3">{children}</p>
-}
-
-const ServiceContent = ({
-  children,
-  variant,
-}: {
-  children: React.ReactNode
-  variant: 'left' | 'right'
-}) => {
-  return (
-    <div
-      className={cn(
-        'max-w-full px-12 md:px-0 md:max-w-[400px]',
-        variant == 'right' && 'md:ml-auto'
-      )}
-    >
-      {children}
-    </div>
-  )
-}
-
-const ServiceCTA = ({ url, urlText }: { url: string; urlText: string }) => {
-  return (
-    <Link
-      href={url}
-      className="inline-block bg-[#CE2939] text-white transition-transform duration-200 text-md px-4 py-2 rounded-full mt-4 hover:translate-x-2 hover:-translate-y-1"
-    >
-      {urlText}
-    </Link>
-  )
-}
 const WorkWithUsStep = () => {
   return (
     <div className="relative mt-10 max-w-[300px] px-4 pt-12 pb-8 mb-8 border-2 border-[#477050] rounded-xl">
---
+++ components/service.tsx
@@ -0,0 +1,90 @@
+import Image, { StaticImageData } from 'next/image'
+import Link from 'next/link'
+
+import { cn } from '@/lib/utils'
+
+const Service = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="max-w-full lg:max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-6 mt-16 mx-0 md:mx-6 lg:mx-auto">
+      {children}
+    </div>
+  )
+}
+
+interface ServiceImageProps {
+  src: StaticImageData
+  alt: string
+  width: number
+  height: number
+  variant: 'left' | 'right'
+  className?: string
+}
+
+const ServiceImage = (props: ServiceImageProps) => {
+  return (
+    <div
+      className={cn(
+        'order-last px-12 md:px-0',
+        props.variant == 'left' && 'md:order-first'
+      )}
+    >
+      <Image
+        src={props.src}
+        alt={props.alt}
+        width={props.width}
+        height={props.height}
+        className={cn(
+          'max-w-full md:max-w-[350px] mx-auto',
+          props.variant === 'left' ? 'lg:ml-0' : 'lg:mr-0',
+          props.className
+        )}
+      />
+    </div>
+  )
+}
+
+const ServiceTitle = ({ children }: { children: React.ReactNode }) => {
+  return <h3 className="text-2xl font-bold">{children}</h3>
+}
+
+const ServiceDescription = ({ children }: { children: React.ReactNode }) => {
+  return <p className="mt-3">{children}</p>
+}
+
+const ServiceContent = ({
+  children,
+  variant,
+}: {
+  children: React.ReactNode
+  variant: 'left' | 'right'
+}) => {
+  return (
+    <div
+      className={cn(
+        'max-w-full px-12 md:px-0 md:max-w-[400px]',
+        variant == 'right' && 'md:ml-auto'
+      )}
+    >
+      {children}
+    </div>
+  )
+}
+
+const ServiceCTA = ({ url, urlText }: { url: string; urlText: string }) => {
+  return (
+    <Link
+      href={url}
+      className="inline-block bg-[#CE2939] text-white transition-transform duration-200 text-md px-4 py-2 rounded-full mt-4 hover:translate-x-2 hover:-translate-y-1"
+    >
+      {urlText}
+    </Link>
+  )
+}
+
+Service.Image = ServiceImage
+Service.Title = ServiceTitle
+Service.Description = ServiceDescription
+Service.Content = ServiceContent
+Service.CallToAction = ServiceCTA
+
+export default Service
